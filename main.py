312baad06d1a97d492576a4e5a92804a4deb582e
mo-yoda <monareichel.mr@gmail.com>
2023-01-03 18:05:42 +0100

    previous function split into xml import and accessing gene_name

--- main.py
+++ main.py
@@ -1,26 +1,36 @@
 import requests
+import xml.etree.ElementTree as ET
 
-def get_gene_name(accession_number):
-    # Send a GET request to the UniProt API
+
+# function to import xml data
+def import_xml(accesssion_number):
     url = "https://www.uniprot.org/uniprot/" + accession_number + ".xml"
     r = requests.get(url)
     xml_data = r.text
-    #print(xml_data)
 
     # Parse the XML data
-    import xml.etree.ElementTree as ET
     root = ET.fromstring(xml_data)
 
     # Find the entry element
     entry_element = root.find('./{http://uniprot.org/uniprot}entry')
 
-    # Find the primary gene name
+    gene_name = get_gene_name(entry_element)
+
+    return(gene_name)
+
+# function to find gene_name in entry
+
+def get_gene_name(entry_element):
     for name in entry_element.findall(
             './{http://uniprot.org/uniprot}gene/{http://uniprot.org/uniprot}name[@type="primary"]'):
         primary_gene_name = name.text
         return(primary_gene_name)
 
+
+
+
 # Test the function
 accession_number = "P49407"
-gene_name = get_gene_name(accession_number)
-print(gene_name)
+test = import_xml(accession_number)
+
+print(test)
