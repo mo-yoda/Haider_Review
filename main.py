04be53eed616e8c0467af76a04f72ad54ad07dc8
mo-yoda <monareichel.mr@gmail.com>
2023-01-03 18:41:07 +0100

    added uniprotList function to import .xlsx with accession numbers to be used

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
 import requests
+import openpyxl
 import xml.etree.ElementTree as ET
+import pandas as pd
+
+path = 'C:/Users/monar/Google Drive/Arbeit/homeoffice/230103_RH review/barr1+2 interactome/python_test/'
+file = "access.xlsx"
+column = "ID"
+
+
+
+# function to import xlsx file with accession numbers
+def uniprotList(path, file, column):
+    df = pd.read_excel(io = path + file, engine = "openpyxl")
+    ID_list = df[column]
+    return (ID_list)
+
+
+
+# function to get information from a list of accession numbers
 
 
 # function to import xml data
-def import_xml(accesssion_number):
+def import_xml(accession_number):
     url = "https://www.uniprot.org/uniprot/" + accession_number + ".xml"
     r = requests.get(url)
     xml_data = r.text
@@ -16,21 +34,23 @@ def import_xml(accesssion_number):
 
     gene_name = get_gene_name(entry_element)
 
-    return(gene_name)
+    return (gene_name)
+
 
 # function to find gene_name in entry
-
 def get_gene_name(entry_element):
     for name in entry_element.findall(
             './{http://uniprot.org/uniprot}gene/{http://uniprot.org/uniprot}name[@type="primary"]'):
         primary_gene_name = name.text
-        return(primary_gene_name)
-
-
-
+        return (primary_gene_name)
 
 # Test the function
-accession_number = "P49407"
-test = import_xml(accession_number)
+#accession_number = "P49407"
+#test = import_xml(accession_number)
 
-print(test)
+#print(test)
+
+
+# test
+test2 = uniprotList(path, file, column)
+print(test2)
