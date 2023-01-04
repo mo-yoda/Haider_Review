ed8f34fb30ff1da2453fc85cecdfc1e00d08cc78
mo-yoda <monareichel.mr@gmail.com>
2023-01-04 13:44:10 +0100

    function to extract EC number

--- main.py
+++ main.py
@@ -15,18 +15,18 @@ def uniprotList(path, file, column):
     result_df = get_all_info(ID_list)
     return (result_df)
 
+
 # function to get information from a list of accession numbers
 def get_all_info(ID_list):
     result_df = pd.DataFrame()
     for ID in ID_list:
         # adds all information gathered with import_xml to new column in result_df
-        result_df[ID] = pd.Series(import_xml(ID), name = ID)
+        result_df[ID] = pd.Series(import_xml(ID), name=ID)
     # transpose df -> row per accession number and add column titels
     result_df = result_df.transpose()
     result_df = result_df.reset_index(level=0)
-    result_df.columns = ["ID", "Gene Name", "Species", "dummy"] ### !!! edit this according to extracted info!
-    return(result_df)
-
+    result_df.columns = ["ID", "Gene Name", "Species", "EC Number", "dummy"]  ### !!! edit this according to extracted info!
+    return (result_df)
 
 
 # function to import xml data
@@ -44,9 +44,10 @@ def import_xml(accession_number):
     # extract certain information, for each own function
     gene_name = get_gene_name(entry_element)
     species = get_species(entry_element)
+    ec_number = get_ec(entry_element)
     add_info = "dummy"
 
-    return([gene_name, species, add_info])
+    return ([gene_name, species, ec_number, add_info])
 
 
 # function to find gene_name in entry
@@ -64,23 +65,37 @@ def get_species(entry_element):
         scientific_species = species.text
         return (scientific_species)
 
+
+# function to find EC number in entry
+def get_ec(entry_element):
+    # if there is no entry, e.g. no ecNumber since protein is not an enzyme
+    ec_number = 0
+    if not entry_element.findall(
+            './{http://uniprot.org/uniprot}protein/{http://uniprot.org/uniprot}recommendedName/{http://uniprot.org/uniprot}ecNumber'):
+        print("no ec")
+        ec_number = ["NA"]
+    else:
+        for ec in entry_element.findall(
+                './{http://uniprot.org/uniprot}protein/{http://uniprot.org/uniprot}recommendedName/{http://uniprot.org/uniprot}ecNumber'):
+            print(ec.text)
+            ec_number = ec.text
+    return ec_number
+
+
+
 ### TODO:
 # ?? get GO terms (are a lot for one protein) -> see if there are online tools
 # get poss. catalytic activity -> ecNumber --> if its present, it is an enzyme
 # get Keywords (only "biological function")
 
 
-
 # function to export result, main function
 def export_xlsx(path, file, column):
     result_df = uniprotList(path, file, column)
-    result_df.to_excel(path+file[:-5]+"_result.xlsx")
+    result_df.to_excel(path + file[:-5] + "_result.xlsx")
     print(result_df)
-    return(print("Finished"))
+    return (print("Finished"))
 
 
-
-
-
-#test2
+# test2
 export_xlsx(path, file, column)
