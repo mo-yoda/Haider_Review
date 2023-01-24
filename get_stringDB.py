ea186a04ad04011b4228e1409bf902a5303dbb52
mo-yoda <monareichel@gmail.com>
2023-01-24 16:17:00 +0100

    interaction partners are exported as .xlsx

--- get_stringDB.py
+++ get_stringDB.py
@@ -1,4 +1,3 @@
-import xml.etree.ElementTree as ET
 import pandas as pd
 import requests
 import io
@@ -8,12 +7,11 @@ import io
 # IMZ path
 path = 'B:/FuL/IMZ01/Hoffmann/Personal data folders/Mona/Paper/XXX_Haider et al_Review/barr1+2 interactome/stringDB_data/'
 
-
 # HELP
 # https://string-db.org/help/api/
 
 # what we need:
-#/api/tsv/interaction_partners? -> Gets all the STRING interaction partners of your proteins
+# /api/tsv/interaction_partners? -> Gets all the STRING interaction partners of your proteins
 
 
 # api parameters, see help site for options
@@ -33,22 +31,38 @@ print(request_url)
 
 # set parameters for request
 params = {
-    "identifiers" : "%0d".join(my_genes),
-    "species" : 9606, # species NCBI identifier for human
-    "limit" : 1000, # limits the number of interaction proteins retrieved per protein
-    "caller_identity" : "Mona" # my identifier for stringDB
+    "identifiers": "%0d".join(my_genes),
+    "species": 9606,  # species NCBI identifier for human
+    "limit": 1000,  # limits the number of interaction proteins retrieved per protein
+    "caller_identity": "Mona"  # my identifier for stringDB
 }
 
 # call STRING request
-response = requests.post(request_url, data = params)
+response = requests.post(request_url, data=params)
 
 # Read and parse the results
+# see help for all option of scores which can be retrieved
+temp_row = str()
+data_string = str()
 for line in response.text.strip().split("\n"):
     l = line.strip().split("\t")
     query_ensp = l[0]
     query_name = l[2]
     partner_ensp = l[1]
     partner_name = l[3]
-    combined_score = l[5]
-    print("\t".join([query_ensp, query_name, partner_name, combined_score]))
+    combined_score = l[5]  # in help called "score"
+    text_score = l[12]  # textmining score (aus interesse)
+    temp_row = "\t".join([query_ensp, query_name,
+                          partner_ensp, partner_name,
+                          combined_score, text_score])
+    data_string += temp_row + "\n"
 
+# string to dataframe
+df = pd.read_csv(io.StringIO(data_string), sep="\t")
+
+df.to_excel(path + "test.xlsx")
+
+##  about the combined_score
+# The combined score is computed by combining the probabilities from the different evidence channels and
+# corrected for the probability of randomly observing an interaction. For a more detailed description please
+# see von Mering, et al. Nucleic Acids Res. 2005
