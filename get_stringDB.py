9b31bd80dffa84175c641fd584b97e680e669647
mo-yoda <monareichel@gmail.com>
2023-01-24 15:07:02 +0100

    initital commit for stringDB

---
+++ get_stringDB.py
@@ -0,0 +1,11 @@
+import xml.etree.ElementTree as ET
+import pandas as pd
+import requests
+import io
+
+# homeoffice path
+# path = 'C:/Users/monar/Google Drive/Arbeit/homeoffice/230103_RH review/barr1+2 interactome/stringDB_data/'
+# IMZ path
+path = 'B:/FuL/IMZ01/Hoffmann/Personal data folders/Mona/Paper/XXX_Haider et al_Review/barr1+2 interactome/stringDB_data/'
+
+
