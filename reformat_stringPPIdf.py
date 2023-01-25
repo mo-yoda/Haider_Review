3b69c5301003dd238dcfa4915d5e0a263543dc44
mo-yoda <monareichel@gmail.com>
2023-01-25 15:41:08 +0100

    separate DFs according to "uniqueness", export as separate xlsx

--- reformat_stringPPIdf.py
+++ reformat_stringPPIdf.py
@@ -9,8 +9,8 @@ path = 'B:/FuL/IMZ01/Hoffmann/Personal data folders/Mona/Paper/XXX_Haider et al_
 all_df = pd.read_excel(path + "interactors_stringDB.xlsx")
 all_df.rename(columns = {all_df.columns[0]: "index"}, inplace=True)
 
-## reformat retrieved interactors
-# identify non-unique interactors, add columns with unique or non-unique
+#### reformat retrieved interactors
+### identify non-unique interactors, add columns with unique or non-unique
 print(all_df.columns)
 print(all_df.shape)
 
@@ -27,14 +27,36 @@ for i, e in enumerate(all_dupl):
         unique += [all_df['preferredName_A'][i]]
 unique = pd.Series(unique)
 
-
 # add this series as new column to data
 all_df_uniq = pd.concat([all_df, unique], axis = 1)
 all_df_uniq.rename(columns = {0 : "uniqueness"}, inplace=True)
-print(all_df_uniq)
 
+# export xlsx with all interactors
+all_df_uniq.to_excel(path + "all_interactors.xlsx")
 
+# create separate DFs based on "uniqueness" column
+arrb1_only = all_df_uniq[all_df_uniq["uniqueness"] == "ARRB1"]
+arrb2_only = all_df_uniq[all_df_uniq["uniqueness"] == "ARRB2"]
+both_arrb = all_df_uniq[all_df_uniq["uniqueness"] == "both"]
 
+# export all DFs
+all_df_uniq.to_excel(path + "all_interactors.xlsx")
+arrb1_only.to_excel(path + "ARRB1_interactors.xlsx")
+arrb2_only.to_excel(path + "ARRB2_interactors.xlsx")
+both_arrb.to_excel(path + "interactors_both_ARRB.xlsx")
 
-# TODO
-# add function to export bArr1, bArr2 interaction proteins separately
+# print dataset stats
+print("-----------------------")
+print("barr1 only interactors")
+print(len(arrb1_only))
+print("barr2 only interactors")
+print(len(arrb2_only))
+
+print("interactors of both barr")
+print(len(both_arrb))
+
+print("-----------------------")
+print("barr1 interactors")
+print(len(all_df[all_df["preferredName_A"] == "ARRB1"]))
+print("barr2 interactors")
+print(len(all_df[all_df["preferredName_A"] == "ARRB2"]))
