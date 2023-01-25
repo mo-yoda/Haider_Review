d2a0b922096538f4a4c437d6e62de7143ee48e46
mo-yoda <monareichel@gmail.com>
2023-01-25 13:58:21 +0100

    initial commit, script to reformat retrieved interacting partners by get_stringDB.py

---
+++ reformat_stringPPIdf.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+## reformat retrieved interactors
+# identify non-unique interactors
+
+
+
+
+
+
+# TODO
+# generally, recode with functions
+# add function to export bArr1, bArr2 interaction proteins separately
+# add function to separate unique vs non-unique interaction proteins
+# -> add factor as column + export in separate sheets of one xlsx
