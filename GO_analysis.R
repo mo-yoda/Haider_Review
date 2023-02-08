c356cb8ff3a1adc1a0efc1c390dafa10277cce4b
mo-yoda <monareichel@gmail.com>
2023-02-08 20:19:49 +0100

    data import as function

--- GO_analysis.R
+++ GO_analysis.R
@@ -11,16 +11,19 @@ lapply(wants, require, character.only = TRUE)
 path <- 'B:/FuL/IMZ01/Hoffmann/Personal data folders/Mona/Paper/XXX_Haider et al_Review/barr1+2 interactome/stringDB_data/GO_analysis/'
 setwd(path)
 
-##### import data #####
-file_names = list.files(pattern="*.xlsx", full.names = TRUE)
-file_names <- file_names[which(str_detect(file_names, "GO"))]
+import_xlsx <- function(filename_pattern) {
+  file_names = list.files(pattern = "*.xlsx", full.names = TRUE)
+  file_names <- file_names[which(str_detect(file_names, filename_pattern))]
 
-# e.g. with GPCR or without
-conditions <- substr(file_names, 3, nchar(file_names)-5)
-
-data <- vector("list")
-i = 1
-for (file in substr(file_names, 1, nchar(file_names))){
-  data[[conditions[i]]] <- read.xlsx(file)
-  i = i +1
+  # e.g. with GPCR or without
+  conditions <- substr(file_names, 3, nchar(file_names) - 5)
+  data <- vector("list")
+  i = 1
+  for (file in substr(file_names, 1, nchar(file_names))) {
+    data[[conditions[i]]] <- read.xlsx(file)
+    i = i + 1
+  }
+  return(data)
 }
+
+data_list <- import_xlsx('GO')
