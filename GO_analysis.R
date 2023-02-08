325624c3bb9efe7cfffb30e6ec1c1afe59abc708
mo-yoda <monareichel@gmail.com>
2023-02-08 20:59:49 +0100

    pseudo code for planned next steps

--- GO_analysis.R
+++ GO_analysis.R
@@ -1,7 +1,7 @@
 # list of packages needed
 wants <- c("openxlsx",
            "stringr")
-has   <- wants %in% rownames(installed.packages())
+has <- wants %in% rownames(installed.packages())
 if(any(!has)) install.packages(wants[!has])
 lapply(wants, require, character.only = TRUE)
 
@@ -27,3 +27,25 @@ import_xlsx <- function(filename_pattern) {
 }
 
 data_list <- import_xlsx('GO')
+
+
+
+test_data <- data_list[[1]]
+
+# count certain GO
+ba1_freq <- table(as.factor(a1[,"GO_ids"]))
+ba2_freq <- table(as.factor(a2[,"GO_ids"]))
+# pseudo code
+freq_df <- data.frame(GO_ids = levels(as.factor(test_data[,"GO_ids"])))
+ba1_col<- c()
+ba2_col <- c()
+# for each level in as.factor(test_data[,"GO_ids"])
+# ba1_col <- ba1_freq[level]
+# ba2_col <- ba2_freq[level]
+# if NA, return 0 # -> wrap this in a function
+# add bar1_col and ba2_col as new cols to freq_df
+
+# then use this df to create matrices for each ID in a loop
+# non-ID counts via -->
+# length(levels(as.factor(a1[,"uniprot_ID_proteinB"]))) - ID_counts
+# (first part above is count of unique proteins in a1
