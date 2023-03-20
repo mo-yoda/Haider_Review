46096a290f0d902eaf81567607ba5bed37d2a6f3
mo-yoda <monareichel@gmail.com>
2023-03-20 10:23:43 +0100

    path to generic path

--- get_stringDB.py
+++ get_stringDB.py
@@ -3,10 +3,7 @@ import pandas as pd
 import requests
 import io
 
-# homeoffice path
-# path = 'C:/Users/monar/Google Drive/Arbeit/homeoffice/230103_RH review/barr1+2 interactome/stringDB_data/'
-# IMZ path
-path = 'B:/FuL/IMZ01/Hoffmann/Personal data folders/Mona/Paper/XXX_Haider et al_Review/barr1+2 interactome/stringDB_data/'
+path = 'path/to/folder'
 
 # define interactors for which proteins should be retrieved:
 # get STRING ENS via uniprot (manually)
